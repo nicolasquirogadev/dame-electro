cfcc7379ae27f6a4640414885b08f66b9d322c86
zicolino <167160363+zicolino@users.noreply.github.com>
2025-07-04 17:16:09 -0300

    fix react-router

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode, useState } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as BrowserRouter, Routes, Route, Link, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
 import './index.css';
 import './nav.css';
 import './mediaqueries.css';
@@ -114,9 +114,9 @@ const AppContent = () => {
 const App = () => {
   return (
     <StrictMode>
-      <BrowserRouter basename="/">
+      <Router basename="/">
         <AppContent />
-      </BrowserRouter>
+      </Router>
     </StrictMode>
   );
 };
